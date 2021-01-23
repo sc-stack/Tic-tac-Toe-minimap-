a7bbc7847ef6d5556111e9795fa79eaeb436fd72
Sammy Chen <chensongsen03@gmail.com>
2021-01-22 22:18:20 -0600

    Game flow initiated

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const Player = (marker, turn, name) => {
+    const getTurn = () => {
+        return turn;
+    }
+    const getMarker = () => {
+        return marker;
+    }
+    const getName = () => {
+        return name;
+    }
+    const changeTurn = () =>{
+        turn = !turn;
+    }
+    return{getTurn, getName, changeTurn};
+};
 let GameBoard = (function(){
+    const player1 = ('x', true, "Player One");
+    const player2 = ('o', false, "Player Two");
     let gameBoard;
     let init = () =>{
         gameBoard =  [
@@ -9,7 +26,11 @@ let GameBoard = (function(){
         makeGrid();
         console.log("has been reset");
     };
-    let makeGrid = () =>{
+    let changeTurns = () =>{
+        player1.changeTurn();
+        player2.changeTurn();
+    }
+    let makeGrid = () => {
         const container = document.querySelector('#container');
         container.setAttribute('style', `grid-template-columns : repeat(${3}, 1fr); grid-template-rows : repeat(${3}, 1fr)`);
         for(let i = 0; i < 3 ; i++){
@@ -23,9 +44,45 @@ let GameBoard = (function(){
         }
 
     }
+    const checkWin = () =>{
+        //horizontals and vertical
+        for(let i = 0; i < gameBoard.length; i++){
+            let row = gameBoard[i];
+            if(gameBoard[i][0] == gameBoard[i][1] == gameBoard[i][2]){
+                return true;
+            }
+            for(let x = 0; x< row.length; x++){
+                if(gameBoard[0][x] && gameBoard[1][x] && gameBoard[2][x]){
+                    return true;
+                }
+            }
+        }
+    }
+    //runs the game
+    const runGame = () => {
+        const divs = document.querySelectorAll('div');
+        for(let i = 0; i < divs.length; i++){
+            if(checkWin()){
+                break;
+            }
+            const div = divs[i];
+            div.addEventListener('click', () => {
+                const column = div.getAttribute('data-column');
+                const row = div.getAttribute('data-row');
+                let turn = player1.getTurn(); 
+                if(gameBoard[row][column] == "" && turn){
+                    div.innerHTML = player1.getMarker();
+                    changeTurns();
+                }else if(gameBoard[row][column] == "" && !turn){
+                    div.innerHTML = player2.getMarker();
+                    changeTurns();
+                }
+            });
+        }
+    }
     return{
-        init,
+        init, runGame,
     };
 })();
 GameBoard.init();
-//const Player = (name, )
+GameBoard.runGame();
